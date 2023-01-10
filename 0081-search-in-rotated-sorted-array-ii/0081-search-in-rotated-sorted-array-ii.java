8263d549285d1a784f70aa07445a1d37af50cd51
Manan Sharma <111525730+MananSharma2710@users.noreply.github.com>
2023-01-10 15:02:34 +0530

    Time: 0 ms (100.00%), Space: 42 MB (88.97%) - LeetHub

--- 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -27,7 +27,7 @@ class Solution {
             int mid = start + (end-start)/2;
             if(mid < end && arr[mid] > arr[mid+1])
                 return mid;
-            if (mid> start && arr[mid]< arr[mid-1])
+            else if (mid> start && arr[mid]< arr[mid-1])
                 return mid-1;
             if(arr[start]== arr[mid] && arr[mid]== arr[end]){
                 if(start<arr.length-1 && arr[start]>arr[start+1])
